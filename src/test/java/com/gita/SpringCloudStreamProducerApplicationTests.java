c1d401a377fbe602e17f3bf0379671afee5627bc
Gita Mittala <gita.mittala@nordstrom.com>
2020-06-25 22:32:51 -0700

    spring cloud stream producer

---
+++ src/test/java/com/gita/SpringCloudStreamProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
